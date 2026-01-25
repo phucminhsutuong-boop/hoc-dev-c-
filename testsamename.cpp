5044e0fd77d211d228be58328bf5e9b70e0a4aa5
phucminhsutuong-boop <phucminhsutuong@gmail.com>
2026-01-25 21:54:06 +0700

    testsamename

---
+++ testsamename.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+	char a[100],s[100];
+	scanf("%s%s",a,s);
+	if(strcmp(a,s)==0) printf("same");
+	else printf("not same");
+	return 0;
+} // 25/1/2026 bai nay hay, minh chua giai dc phai xem loi giai
