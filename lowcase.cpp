876fa08cd0314418a8441b984d9c3d1eab2d4d40
phucminhsutuong-boop <phucminhsutuong@gmail.com>
2026-01-25 20:58:17 +0700

    lowcase

--- lowcase.cpp
+++ lowcase.cpp
@@ -5,9 +5,9 @@ int main(){
 	char s[100];
 	scanf("%s",s);
 	for(int i=0;i<strlen(s);i++){
-		if(s[i]>=65 & s[i]<=97)
+		if(s[i]>=65 & s[i]<=90)
 		s[i]+=32;
 	}
 	printf("%s",s);
 		return 0;
-} // 25/1/2026
+} // 25/1/2026 
