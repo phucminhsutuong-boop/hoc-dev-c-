f39ee3b32eb589172652f43988e18f89f93b0bdf
phucminhsutuong-boop <phucminhsutuong@gmail.com>
2026-01-26 11:17:40 +0700

    hamsogiamdan

---
+++ hamsogiamdan.cpp
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+void nhap(int a[], int n){
+    for(int i=0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+}
+int gan;
+int sosanh(int a[], int n){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(a[i]>a[j]){
+            gan=a[i];
+            a[i]=a[j];
+            a[j]=gan;	
+			}
+            
+        }
+    }
+}
+int in(int a[], int n){
+    for(int j=0;j<n;j++){
+        printf("%d ",a[j]);
+    }
+}
+int main(){
+    int n;
+    scanf("%d",&n);
+    int a[n];
+    nhap(a,n);
+    sosanh(a,n);
+    in(a,n);
+    return 0;    
+}
