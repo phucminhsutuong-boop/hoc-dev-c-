111da304fed9e01f771fb0ddfc04c923f1261046
phucminhsutuong-boop <phucminhsutuong@gmail.com>
2026-01-26 09:58:53 +0700

    viethamnhap

---
+++ viethamnhap.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+ 	
+ 	void nhap(char s[100]){
+ 		scanf("%s",s);
+ 		printf("Hello %s",s);
+ 		
+	 }
+	 int main(){
+	 	char s[100];
+	 	nhap(s);
+	 	return 0;
+	 } // 26/1/2026
